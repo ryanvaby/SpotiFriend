8a971afffc7e0e109f800bfe17dcd2630cbcec30
Your Name <ryan.aby@outlook.com>
2024-04-23 09:19:46 -0700

    main.js updates

--- main.js
+++ main.js
@@ -1,52 +1,84 @@
-/*
-var Spotify = require('spotify-web-api-js');
-var s = new Spotify();
+const clientId = 'e667cde587c544508d5926555e9bfc39'; // your clientId
+const redirectUrl = 'eg:http://localhost:8080';        // your redirect URL - must be localhost URL and/or HTTPS
 
-var spotifyApi = new SpotifyWebApi();
-spotifyApi.setAccessToken('<here_your_access_token>');*/
+const authorizationEndpoint = "https://accounts.spotify.com/authorize";
+const tokenEndpoint = "https://accounts.spotify.com/api/token";
+const scope = 'user-read-private user-read-email';
 
-const generateRandomString = (length) => {
-    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    const values = crypto.getRandomValues(new Uint8Array(length));
-    return values.reduce((acc, x) => acc + possible[x % possible.length], "");
-    console.log("hello1");
+// Data structure that manages the current active token, caching it in localStorage
+const currentToken = {
+  get access_token() { return localStorage.getItem('access_token') || null; },
+  get refresh_token() { return localStorage.getItem('refresh_token') || null; },
+  get expires_in() { return localStorage.getItem('refresh_in') || null },
+  get expires() { return localStorage.getItem('expires') || null },
+
+  save: function (response) {
+    const { access_token, refresh_token, expires_in } = response;
+    localStorage.setItem('access_token', access_token);
+    localStorage.setItem('refresh_token', refresh_token);
+    localStorage.setItem('expires_in', expires_in);
+
+    const now = new Date();
+    const expiry = new Date(now.getTime() + (expires_in * 1000));
+    localStorage.setItem('expires', expiry);
   }
-  
-const codeVerifier  = generateRandomString(64);
+};
 
-const sha256 = async (plain) => {
-  const encoder = new TextEncoder()
-  const data = encoder.encode(plain)
-  return window.crypto.subtle.digest('SHA-256', data)
+// On page load, try to fetch auth code from current browser search URL
+const args = new URLSearchParams(window.location.search);
+const code = args.get('code');
+
+// If we find a code, we're in a callback, do a token exchange
+if (code) {
+  const token = await getToken(code);
+  currentToken.save(token);
+
+  // Remove code from URL so we can refresh correctly.
+  const url = new URL(window.location.href);
+  url.searchParams.delete("code");
+
+  const updatedUrl = url.search ? url.href : url.href.replace('?', '');
+  window.history.replaceState({}, document.title, updatedUrl);
 }
 
-const base64encode = (input) => {
-  return btoa(String.fromCharCode(...new Uint8Array(input)))
+// If we have a token, we're logged in, so fetch user data and render logged in template
+if (currentToken.access_token) {
+  const userData = await getUserData();
+  renderTemplate("main", "logged-in-template", userData);
+  renderTemplate("oauth", "oauth-template", currentToken);
+}
+
+// Otherwise we're not logged in, so render the login template
+if (!currentToken.access_token) {
+  renderTemplate("main", "login");
+}
+
+async function redirectToSpotifyAuthorize() {
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const randomValues = crypto.getRandomValues(new Uint8Array(64));
+  const randomString = randomValues.reduce((acc, x) => acc + possible[x % possible.length], "");
+
+  const code_verifier = randomString;
+  const data = new TextEncoder().encode(code_verifier);
+  const hashed = await crypto.subtle.digest('SHA-256', data);
+
+  const code_challenge_base64 = btoa(String.fromCharCode(...new Uint8Array(hashed)))
     .replace(/=/g, '')
     .replace(/\+/g, '-')
     .replace(/\//g, '_');
+
+  window.localStorage.setItem('code_verifier', code_verifier);
+
+  const authUrl = new URL(authorizationEndpoint)
+  const params = {
+    response_type: 'code',
+    client_id: clientId,
+    scope: scope,
+    code_challenge_method: 'S256',
+    code_challenge: code_challenge_base64,
+    redirect_uri: redirectUrl,
+  };
+
+  authUrl.search = new URLSearchParams(params).toString();
+  window.location.href = authUrl.toString(); // Redirect the user to the authorization server for login
 }
-
-const hashed = await sha256(codeVerifier)
-const codeChallenge = base64encode(hashed);
-
-const clientId = 'YOUR_CLIENT_ID';
-const redirectUri = 'http://localhost:8080';
-
-const scope = 'user-read-private user-read-email';
-const authUrl = new URL("https://accounts.spotify.com/authorize")
-
-// generated in the previous step
-window.localStorage.setItem('code_verifier', codeVerifier);
-
-const params =  {
-  response_type: 'code',
-  client_id: clientId,
-  scope,
-  code_challenge_method: 'S256',
-  code_challenge: codeChallenge,
-  redirect_uri: redirectUri,
-}
-
-authUrl.search = new URLSearchParams(params).toString();
-window.location.href = authUrl.toString();
